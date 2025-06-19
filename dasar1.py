188d5f8bf0b20bf85add7b47842f127d9e4232ba
zotacloud-luma <loud.zotac@gmail.com>
2025-06-19 15:57:19 +0700

    First time learning Python

---
+++ dasar1.py
@@ -0,0 +1,13 @@
+# Misi Astreon Hari 1 - Dasar Python
+
+# Variabel & Tipe Data
+nama = "Astreon"
+umur = 2025
+tinggi = 1.75
+aktif = True
+
+# Print
+print("Halo, saya", nama)
+print("Tahun saat ini:", umur)
+print("Tinggi saya:", tinggi)
+print("Apakah saya aktif?", aktif)
